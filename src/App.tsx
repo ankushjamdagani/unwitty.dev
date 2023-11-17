5443443927e9bb9f46b4606b7cd05c0478f9f77b
Ankush Jamdagani <anqushjamdagani@gmail.com>
2023-11-17 19:26:01 +0530

    dynamic dpr based on fps

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
-import {
-  BakeShadows,
-  Environment,
-  Lightformer,
-  OrbitControls,
-  Sky,
-  Stage,
-} from "@react-three/drei";
+import { OrbitControls, PerformanceMonitor, Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
 import { useControls } from "leva";
 
 import Terrain from "./components/Terrain";
+import { useState } from "react";
 
 export default function App() {
+  const [perfSucks, degrade] = useState(false);
+
   const { sunPosition } = useControls("sky", {
     sunPosition: { value: [1, 2, 3] },
   });
 
   return (
-    <Canvas shadows>
+    <Canvas dpr={[1, perfSucks ? 1.5 : 2]} shadows>
       <Perf position="top-left" />
       <OrbitControls />
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
+
+      <PerformanceMonitor
+        flipflops={3}
+        onIncline={() => {
+          console.log("performance - onIncline");
+          degrade(false);
+        }}
+        onDecline={() => {
+          console.log("performance - onDecline");
+          degrade(true);
+        }}
+      />
 
       <Sky sunPosition={sunPosition} />
       <Terrain />
