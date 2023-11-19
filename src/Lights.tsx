ead9319a9f24db664b53cc47df2951b0074b43f9
Ankush Jamdagani <anqushjamdagani@gmail.com>
2023-11-19 15:23:01 +0530

    minor cleanup

--- src/Lights.tsx
+++ src/Lights.tsx
@@ -5,7 +5,10 @@ import { useControls } from "leva";
 
 function Lights() {
   const pointLightRef = useRef();
+  const directionalLightRef = useRef();
+
   useHelper(pointLightRef, THREE.PointLightHelper, 0.2, "red");
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, "red");
 
   const { pointLight, ambientLight } = useControls("light", {
     ambientLight: 0.2,
@@ -15,6 +18,7 @@ function Lights() {
   return (
     <>
       <directionalLight
+        ref={directionalLightRef}
         position={[-20, 2, 10]}
         intensity={2.5}
         color={"blue"}
