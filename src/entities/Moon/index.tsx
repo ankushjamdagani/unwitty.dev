02e93450e12ff6cf71b45d08a29955ddc1b406c7
Ankush Jamdagani <anqushjamdagani@gmail.com>
2023-11-21 23:48:51 +0530

    lazy moon

--- src/entities/Moon/index.tsx
+++ src/entities/Moon/index.tsx
@@ -1,19 +1,24 @@
 import { useGLTF } from "@react-three/drei";
 import { BallCollider, RigidBody } from "@react-three/rapier";
+import { Suspense, useMemo } from "react";
 
 function Moon() {
   const { nodes, materials } = useGLTF("./models/moon_nasa.glb");
 
+  const loadingCube = useMemo(() => <mesh geometry={<sphereGeometry />} />, []);
+
   return (
     <>
       <RigidBody type="fixed" colliders={false}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Cube008.geometry}
-          material={materials["Default OBJ.005"]}
-          scale={0.02}
-        />
+        <Suspense fallback={loadingCube}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube008.geometry}
+            material={materials["Default OBJ.005"]}
+            scale={0.02}
+          />
+        </Suspense>
         <BallCollider args={[10.1]} mass={Infinity} />
       </RigidBody>
     </>
