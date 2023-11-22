a88595179c43d3db68ea5c3f68322e5974721be3
Ankush Jamdagani <anqushjamdagani@gmail.com>
2023-11-23 02:47:14 +0530

    player2 moved a bit

--- src/entities/Player2/index.tsx
+++ src/entities/Player2/index.tsx
@@ -1,9 +1,16 @@
-import { useControls } from "leva";
 import { useRef } from "react";
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { useControls } from "leva";
+
+const velocity = new THREE.Vector2(1, 1);
 
 function Player2() {
   const playerRef = useRef();
   const spotLightRef = useRef();
+
+  const [, get] = useKeyboardControls();
 
   const { color, intensity, distance, angle, penumbra, decay } = useControls(
     "Player Headlight",
@@ -17,9 +24,36 @@ function Player2() {
     }
   );
 
+  useFrame((state, delta) => {
+    const player = playerRef.current;
+
+    if (!player) return;
+
+    const { forward, back, left, right } = get();
+
+    // console.log(player.position);
+
+    const time = state.clock.elapsedTime;
+
+    if (left) velocity.x = -1;
+    if (right) velocity.x = 1;
+
+    const x = velocity.x * Math.sin(time) * 20;
+    const z = velocity.y * Math.cos(time) * 20;
+
+    console.log(x, z, velocity);
+
+    player.position.set(x, 2, z);
+
+    const vectorPos = player.position;
+
+    // update torch
+    spotLightRef.current.position.set(vectorPos.x, vectorPos.y, vectorPos.z);
+  });
+
   return (
     <>
-      <mesh ref={playerRef} position={[0, 2, 22]}>
+      <mesh ref={playerRef} position={[5, 2, 22]}>
         <boxGeometry args={[4, 2, 2]} />
         <meshBasicMaterial color={"red"} />
       </mesh>
