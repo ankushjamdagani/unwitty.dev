731507a026d352ba5f128ef207b8a34bb289d5d4
Ankush Jamdagani <anqushjamdagani@gmail.com>
2023-12-27 01:55:20 +0530

    web: functional navigation

--- apps/web/src/app/_components/Nav/Nav.tsx
+++ apps/web/src/app/_components/Nav/Nav.tsx
@@ -1,13 +1,49 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+
+import "./Nav.styles.css";
+import { useState } from "react";
+
+const Links = [
+  {
+    path: "#info",
+    label: "Info",
+  },
+  {
+    path: "#projects",
+    label: "Experiments",
+  },
+  {
+    path: "#words",
+    label: "Thoughts",
+  },
+  {
+    path: "#work",
+    label: "Work",
+  },
+];
 
 export function Nav() {
+  const [activeLink, setActiveLink] = useState(window.location.hash);
+
   return (
     <nav>
-      <Link href={"#info"}>Info</Link>
-      <Link href={"#projects"}>Projects</Link>
-      <Link href={"#blogs"}>Blogs</Link>
-      <Link href={"#work"}>Work</Link>
+      <ul>
+        {Links.map((link) => (
+          <li key={link.path}>
+            <Link
+              href={link.path}
+              prefetch={false}
+              onClick={() => setActiveLink(link.path)}
+              className={activeLink === link.path ? "active" : ""}
+              aria-current={activeLink === link.path}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
