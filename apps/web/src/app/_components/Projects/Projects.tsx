4392cb61f1867713b554d570d2366ccced7ffc17
Ankush Jamdagani <anqushjamdagani@gmail.com>
2023-12-28 01:07:20 +0530

    web: added projects basic structure

--- apps/web/src/app/_components/Projects/Projects.tsx
+++ apps/web/src/app/_components/Projects/Projects.tsx
@@ -1,5 +1,71 @@
-import React from "react";
+import Link from "next/link";
+
+import "./Projects.styles.css";
+
+const List = [
+  {
+    name: "project 1",
+    path: "project 1",
+    thumbnail: {
+      src: ".",
+      type: "gif",
+    },
+  },
+  {
+    name: "project 2",
+    path: "project 2",
+    thumbnail: {
+      src: ".",
+      type: "gif",
+    },
+  },
+  {
+    name: "project 3",
+    path: "project 3",
+    thumbnail: {
+      src: ".",
+      type: "gif",
+    },
+  },
+  {
+    name: "project 4",
+    path: "project 4",
+    thumbnail: {
+      src: ".",
+      type: "gif",
+    },
+  },
+  {
+    name: "project 5",
+    path: "project 5",
+    thumbnail: {
+      src: ".",
+      type: "gif",
+    },
+  },
+  {
+    name: "project 6",
+    path: "project 6",
+    thumbnail: {
+      src: ".",
+      type: "gif",
+    },
+  },
+];
 
 export function Projects() {
-  return <section id="projects">Projects</section>;
+  return (
+    <section id="projects">
+      <header>
+        <h2>Experiments</h2>
+      </header>
+      <ul>
+        {List.map((project) => (
+          <li key={project.path} className={project.name.replace(" ", "-")}>
+            <Link href={`#${project.path}`}>{project.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
