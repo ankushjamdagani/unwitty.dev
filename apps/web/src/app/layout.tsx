abd0694135bdf166e7cc2f0765c90a8324ef0850
Ankush Jamdagani <anqushjamdagani@gmail.com>
2023-12-31 02:55:34 +0530

    web: remove cursor overlay

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className={`${inter.className} mono`}>
         {children}
         {/* <CursorInverted /> */}
-        <CursorDisplacementFire />
+        {/* <CursorDisplacementFire /> */}
         {/* <CursorWind /> */}
       </body>
     </html>
