82f43d68ff3021b2069e8d7ef269a6c103e71b65
Ankush Jamdagani <anqushjamdagani@gmail.com>
2024-01-26 21:07:56 +0530

    web: added breadcrumb

---
+++ apps/web/src/app/projects/layout.tsx
@@ -0,0 +1,21 @@
+import "./projects.styles.css";
+import { BreadCrumb } from "../_components/Breadcrumb";
+
+export default function ProjectsWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="project-wrapper">
+      <BreadCrumb
+        options={[
+          { path: "/", label: "Home" },
+          { path: "/projects", label: "Projects" },
+          { label: "Current" },
+        ]}
+      />
+      <main className="project-item">{children}</main>
+    </div>
+  );
+}
