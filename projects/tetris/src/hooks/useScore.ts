57126f3c6feea295921052d0953f3683280e1116
Ankush Jamdagani <anqushjamdagani@gmail.com>
2024-08-04 18:20:52 +0000

    useScore - added scoreboard and reset

--- projects/tetris/src/hooks/useScore.ts
+++ projects/tetris/src/hooks/useScore.ts
@@ -1,4 +1,6 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
+
+type ScoreBoardItem = { timestamp: number; score: number };
 
 interface useScoreProps {
   defaultState?: {
@@ -13,10 +15,24 @@ const DefaultState = {
 // 1 point for blocks eliminated
 export function useScore({ defaultState = DefaultState }: useScoreProps) {
   const [state, setState] = useState(defaultState);
+  const { score } = state;
+
+  const [scoreBoard, setScoreBoard] = useState<ScoreBoardItem[]>([]);
 
   const addToScore = useCallback((score: number) => {
     setState((state) => ({ ...state, score: state.score + score }));
   }, []);
 
-  return { state, addToScore };
+  const saveToScoreBoard = useCallback(() => {
+    setScoreBoard((state) => [
+      ...state,
+      { timestamp: Date.now(), score: score },
+    ]);
+  }, [score]);
+
+  const reset = useCallback(() => {
+    setState(defaultState);
+  }, [defaultState]);
+
+  return { score, scoreBoard, addToScore, saveToScoreBoard, reset };
 }
