90e08f65fad3a701e7883f4191c6bc4e39ca5034
Ankush Jamdagani <anqushjamdagani@gmail.com>
2025-08-10 03:45:13 +0530

    [portfolio] Favicon updated

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/images/favicon.png" sizes="any" />
       <body className={`${font.className} monos`}>
         <Nav />
         {children}
