356d90083893f848e53e752f8a2de764b4f8e43a
Ankush Jamdagani <anqushjamdagani@gmail.com>
2025-08-21 23:47:24 +0530

    git branches cleanup

---
+++ git-branches-clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "[Start] Clean git branches"
+git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
+echo "[End] Clean git branches"
