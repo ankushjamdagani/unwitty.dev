54d1a4cc72ab4b0215f6db34bd21c4be875bc7d7
Ankush Jamdagani <anqushjamdagani@gmail.com>
2025-09-29 15:57:43 +0530

    project in-progress styles

--- apps/web/src/app/_components/Projects/Projects.tsx
+++ apps/web/src/app/_components/Projects/Projects.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import "./Projects.styles.css";
 
 import { ProjectsMeta, ProjectState, ProjectType } from "@/configs/projects";
+import { LuTriangleAlert } from "react-icons/lu";
 
 const List = [
   ProjectType.GameboyTetris,
@@ -24,7 +25,10 @@ export function Projects() {
             <li key={project} className={`project-item-wrapper ${project}`}>
               {projectConfig.status === ProjectState.InProgress && (
                 <div className="project-in-progress-indicator">
-                  <span>Under Development</span>
+                  <span>
+                    <LuTriangleAlert />
+                    Under Development
+                  </span>
                 </div>
               )}
               <Link
